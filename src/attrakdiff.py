3e6d6e6bfe53bef025de9b0172bf721861e92864
Thibault HILAIRE <thibault.hilaire@lip6.fr>
2022-02-17 16:04:04 +0100

    add licence (comment)

--- src/attrakdiff.py
+++ src/attrakdiff.py
@@ -1,10 +1,42 @@
+""""This file is part of USAT.
+
+	MIT License
+
+	Copyright (c) 2022 - Thibault Hilaire
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+
+USAT is a simple tool that taks survey from Usabilla and plot attrakdiff plots.
+It is written by Thibault Hilaire
+
+File: attrakdiff.py
+Date: Feb 2022
+
+	Plot functions
+"""
+
 
 from typing import Dict, List, TypeVar
 from pandas import read_excel, DataFrame, read_csv
-from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 from math import sqrt
-import seaborn as sns
 from io import BytesIO
 
 from matplotlib.patches import Rectangle
