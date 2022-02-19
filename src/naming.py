069d786c9b57d67aa8e71f84f7fabc3062d6bc63
Thibault HILAIRE <thibault.hilaire@lip6.fr>
2022-02-19 17:56:02 +0100

    remove unnecessary dict

--- src/naming.py
+++ src/naming.py
@@ -34,15 +34,8 @@ Date: Feb 2022
 
 from collections import OrderedDict
 
-
 #TODO: add english (and german) translation
 
-# items = {
-# 	"QP": {"QP1*", "QP2*", "QP3*", "QP4", "QP5*", "QP6", "QP7"},
-# 	"QHS": {"QHS1*", "QHS2", "QHS3*", "QHS4*", "QHS5", "QHS6", "QHS7*"},
-# 	"QHI": {"QHI1", "QHI2*", "QHI3*", "QHI4", "QHI5", "QHI6*", "QHI7"},
-# 	"ATT": {"ATT1*", "ATT2", "ATT3*", "ATT4", "ATT5*", "ATT6", "ATT7*"}
-# }
 
 
 titles = {
